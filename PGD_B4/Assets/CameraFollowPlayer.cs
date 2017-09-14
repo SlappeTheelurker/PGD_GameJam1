277ec968aaf009e19d211bdabf2c63aafdf1316e
Midas - Desktop <midasmilosavljevic@hotmail.com>
2017-09-14 20:13:32 +0200

    .gitignore test, hele project gecommit

---
+++ PGD_B4/Assets/CameraFollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+    public GameObject player;
+
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start ()
+    {
+        offset = player.transform.position - transform.position;
+	}
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.position = new Vector3(player.transform.position.x - offset.x,
+                                         transform.position.y,
+                                         player.transform.position.z - offset.z);
+    }
+}
